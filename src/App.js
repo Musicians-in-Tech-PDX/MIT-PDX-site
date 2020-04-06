a577056ac343acedad6f1b45242d7687d6b73108
AaronMullan <aaroncmullan@gmail.com>
2020-04-06 16:26:57 -0700

    ASCII in effect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import basicLogo from './assets/basic-logo.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={basicLogo} className="App-logo" alt="logo" />
+        {/* <img src={basicLogo} className="App-logo" alt="logo" /> */}
+      <p>.__________________________.<br/>
+    | .___________________. |==|<br/>
+    | |                   | |  |<br/>
+    | |                   | |  |<br/>
+    | |      Musicians    | |  |<br/>
+    | |         in        | |  |<br/>
+    | |        Tech       | |  |<br/>
+    | |         PDX       | |  |<br/>
+    | | ]                 | | ,|<br/>
+    | !___________________! |(c|<br/>
+    !_______________________!__!<br/>
+    |    ___ -=      ___ -= | ,|<br/>
+    | ---[_]---   ---[_]--- |(c|<br/>
+      <br/>
+    !_______________________!__!<br/>
+   /                            \<br/>
+  /  [][][][][][][][][][][][][]  \<br/>
+ /  [][][][][][][][][][][][][][]  \<br/>
+(  [][][][][____________][][][][]  )<br/>
+ \ ------------------------------ /<br/>
+  \______________________________/
+  <br/><br/>
         <a
           className="App-link"
           href="https://twitter.com/musicians_in"
@@ -15,6 +36,7 @@ function App() {
         >
           Follow us on Twitter
         </a>
+  </p><br/>
       </header>
     </div>
   );
